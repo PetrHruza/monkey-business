5e228d5c306cd890db29e3923be38a512df85879
petrhruza <petr.hruza@hotmail.cz>
2022-04-10 21:58:15 +0200

    Initialize project and create Hero component + basic styling.

---
+++ src/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div id="hero">
+      <div className="hero container">
+        <div>
+          <h1>Finworld</h1>
+          <h2>svět peněz</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
