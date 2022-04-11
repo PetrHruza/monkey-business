c0d66b568e02b8f63bca73ecbc288cfa57369656
petrhruza <petr.hruza@hotmail.cz>
2022-04-11 22:53:43 +0200

    Create alternative hero and header.

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import isometricHero from "./img/isometric-hero.png";
+import testHero from "./img/unsplash.jpg";
 
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div id="hero">
-      <div className="hero container">
-        <div>
-          <h1>Finworld</h1>
-          <h2>svět peněz</h2>
+      <div className="hero">
+        <div className="hero-left">
+            {/* <h1>finworld</h1> */}
+        </div>
+        <div className="hero-right">
+          {/* <img src={testHero} alt="Hero Img" /> */}
         </div>
       </div>
     </div>
